f3d98a6f1fed03fcd9347b3c097217fbb7d3df19
Hemedi Abedi Hemedi <abedihemedi002@gmail.com>
2025-11-09 01:12:45 +0300

    troubleshoot css problem

--- books/settings.py
+++ books/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jpnxj9cc1otgl%7w^6kvb@qdl^d3r2w0knm8u3-icd!csb3oj2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
